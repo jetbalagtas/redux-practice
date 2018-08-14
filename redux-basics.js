eba5339f17f3380f510b5b9671227d7df001ec6b
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-14 15:08:09 -0400

    setup reducer and store

---
+++ redux-basics.js
@@ -0,0 +1,23 @@
+// using it as standalone now, executing it with node.js
+
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+
+
+// Dispatching Action
+
+// Subscription
