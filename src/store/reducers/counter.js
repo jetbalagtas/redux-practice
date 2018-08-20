71ae7a85d390f4bb7da67f5fa9f6ebb4cb2b4fbc
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-20 15:28:27 -0400

    use utility functions to make reducers leaner

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   counter: 0
@@ -10,25 +11,13 @@ const reducer = (state = initialState, action) => {
       // const newState = Object.assign({}, state);
       // newState.counter = state.counter + 1
       // return newState;
-    return {
-     ...state,
-     counter: state.counter + 1 
-    }
+      return updateObject(state, {counter: state.counter + 1 });
     case actionTypes.DECREMENT:
-    return {
-      ...state,
-      counter: state.counter - 1
-    }
+      return updateObject(state, {counter: state.counter - 1});
     case actionTypes.ADD:
-    return {
-      ...state,
-      counter: state.counter + action.value
-    }
+      return updateObject(state, {counter: state.counter + action.value});
     case actionTypes.SUBTRACT:
-    return {
-      ...state,
-      counter: state.counter - action.value
-    }
+      return updateObject(state, {counter: state.counter - action.value});
     default:
       return state;
   }
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: []
@@ -7,19 +8,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-    return {
-      ...state,
-      results: state.results.concat({id: new Date(), value: action.result})
-    }
+      return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
     case actionTypes.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1)
-    const udpatedArray = state.results.filter(result => result.id !== action.resultElId);
-    return {
-      ...state,
-      results: udpatedArray
-    }
+      const udpatedArray = state.results.filter(result => result.id !== action.resultElId);
+      return updateObject(state, {results: udpatedArray});
     default:
       return state;
   }
